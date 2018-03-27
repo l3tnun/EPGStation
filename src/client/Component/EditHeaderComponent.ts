58cda698ca4b8bd52ca62b114f6307c46650e1ee
l3tnun <l3tnun@gmail.com>
2018-03-27 21:52:38 +0900

    編集時に表示する EditHeaderComponent を作成

---
+++ src/client/Component/EditHeaderComponent.ts
@@ -0,0 +1,44 @@
+import * as m from 'mithril';
+import Component from './Component';
+
+interface EditHeaderArgs {
+    title: string;
+    button: {
+        onclick(event: Event): void; // click 時に実行される
+        name: string; // material icons name
+    }[];
+    isShow(): boolean; // 表示されているか判定する
+    close(): void; // 非表示処理を行う
+}
+
+/**
+ * EditHeaderComponent
+ */
+class EditHeaderComponent extends Component<EditHeaderArgs> {
+    public view(vnode: m.Vnode<EditHeaderArgs, this>): m.Child | null {
+        if (!vnode.attrs.isShow()) { return null; }
+
+        return m('div', {
+            class: 'edit-header',
+        }, [
+            m('div', { class: 'drawer-button' }, [
+                m('i', {
+                    class: 'material-icons',
+                    onclick: () => { vnode.attrs.close(); },
+                }, 'close'),
+            ]),
+            m('span', { class: 'title' }, vnode.attrs.title),
+            m('div', { class: 'menu-parent' }, [
+                vnode.attrs.button.map((button) => {
+                    return m('label', {
+                        class: 'menu-button header-menu-button mdl-button mdl-js-button mdl-button--icon',
+                        onclick: (event: Event) => { button.onclick(event); },
+                    }, m('i', { class: 'material-icons' }, button.name));
+                }),
+            ]),
+        ]);
+    }
+}
+
+export default EditHeaderComponent;
+
