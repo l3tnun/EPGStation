3cb3f1602bc4993941ac268930a08f8f8dac8f96
l3tnun <l3tnun@gmail.com>
2019-02-16 21:58:47 +0900

    fix

--- src/client/Component/Recorded/RecordedSettingComponent.ts
+++ src/client/Component/Recorded/RecordedSettingComponent.ts
@@ -34,7 +34,7 @@ class RecordedSettingComponent extends Component<void> {
 
         return [
             this.createListItem(
-                'list 表示を有効化する',
+                'リスト表示を有効化する',
                 this.createToggle(
                     () => { return this.viewModel.tmpValue.isEnabledListMode; },
                     (value) => {
