f5735dba363f0215f5a28db68e57b117cf138143
l3tnun <l3tnun@gmail.com>
2019-05-30 21:06:35 +0900

    字幕データを含むマップ処理をデフォルトの設定から削除

--- config/enc.js
+++ config/enc.js
@@ -28,7 +28,8 @@ Array.prototype.push.apply(args,['-i', input]);
 Array.prototype.push.apply(args,['-movflags', 'faststart']);
 
 // 字幕データを含めたストリームをすべてマップ
-Array.prototype.push.apply(args, ['-map', '0', '-ignore_unknown', '-max_muxing_queue_size', maxMuxingQueueSize,]);
+// エンコード失敗報告が多いためコメントアウト
+// Array.prototype.push.apply(args, ['-map', '0', '-ignore_unknown', '-max_muxing_queue_size', maxMuxingQueueSize,]);
 
 // video filter 設定
 let videoFilter = 'yadif';
