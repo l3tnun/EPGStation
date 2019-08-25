72435c9c3a398bcb566eb8c906910b3afb34a1ea
l3tnun <l3tnun@gmail.com>
2019-08-25 17:14:54 +0900

    録画済み部分修正

--- src/client/Component/Recorded/RecordedInfoComponent.ts
+++ src/client/Component/Recorded/RecordedInfoComponent.ts
@@ -66,7 +66,7 @@ class RecordedInfoComponent extends Component<void> {
                 return m('a', {
                     class: 'recorded-link mdl-button mdl-js-button mdl-button--raised mdl-button--colored mdl-js-ripple-effect',
                     onclick: async() => {
-                        if (this.selectViewModel.isEnabledStreaming() && typeof video.encodedId === 'undefined' && !Util.uaIsiOS()) {
+                        if (this.selectViewModel.isEnabledStreaming() && typeof video.encodedId === 'undefined' && !Util.uaIsiOS() && !Util.uaIsiPadOS()) {
                             // TS ストリーミング再生
                             const recorded = this.viewModel.getRecorded();
                             if (recorded === null) { return; }
