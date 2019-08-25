9c6e40aaaa1fa3cc667a9295810f2b33909cd9c2
l3tnun <l3tnun@gmail.com>
2019-08-25 17:45:09 +0900

    HLS URL Scheme に対応

--- src/client/ViewModel/Stream/StreamWatchViewModel.ts
+++ src/client/ViewModel/Stream/StreamWatchViewModel.ts
@@ -85,7 +85,7 @@ class StreamWatchViewModel extends ViewModel {
         let baseURL = setting.customHLSViewerURLScheme;
 
         if (baseURL === null && typeof config.HLSViewer !== 'undefined') {
-            if (Util.uaIsiOS() && typeof config.HLSViewer.ios !== 'undefined') {
+            if ((Util.uaIsiOS() || Util.uaIsiPadOS()) && typeof config.HLSViewer.ios !== 'undefined') {
                 baseURL = config.HLSViewer.ios;
             } else if (Util.uaIsAndroid() && typeof config.HLSViewer.android !== 'undefined') {
                 baseURL = config.HLSViewer.android;
