b52a6e86b51d4fcd0bb0df10581458f8fe457c04
l3tnun <l3tnun@gmail.com>
2020-08-12 01:16:25 +0900

    録画検索メニューの query 生成処理実装 (Recorded 側での query 処理はまだ未実装)

--- client/src/components/recorded/RecordedSearchMenu.vue
+++ client/src/components/recorded/RecordedSearchMenu.vue
@@ -58,6 +58,7 @@ import container from '@/model/ModelContainer';
 import ISocketIOModel from '@/model/socketio/ISocketIOModel';
 import IRecordedSearchState from '@/model/state/recorded/search/IRecordedSearchState';
 import ISnackbarState from '@/model/state/snackbar/ISnackbarState';
+import Util from '@/util/Util';
 import { Component, Vue, Watch } from 'vue-property-decorator';
 import * as apid from '../../../../api';
 
@@ -94,6 +95,32 @@ export default class RecordedSearchMenu extends Vue {
 
     public onSearch(): void {
         this.isOpen = false;
+
+        this.$nextTick(async () => {
+            await Util.sleep(300);
+
+            const searchQuery: any = {};
+            if (typeof this.searchState.keyword !== 'undefined') {
+                searchQuery.keyword = this.searchState.keyword;
+            }
+            if (typeof this.searchState.ruleId !== 'undefined') {
+                searchQuery.ruleId = this.searchState.ruleId;
+            }
+            if (typeof this.searchState.channelId !== 'undefined') {
+                searchQuery.channelId = this.searchState.channelId;
+            }
+            if (typeof this.searchState.genre !== 'undefined') {
+                searchQuery.genre = this.searchState.genre;
+            }
+            if (this.searchState.isOnlyOriginalFile === true) {
+                searchQuery.isOnlyOriginalFile = true;
+            }
+
+            await Util.move(this.$router, {
+                path: '/recorded',
+                query: searchQuery,
+            });
+        });
     }
 
     public onClickMenuBackground(e: Event): boolean {
@@ -122,6 +149,25 @@ export default class RecordedSearchMenu extends Vue {
     public onChangeState(newState: boolean, oldState: boolean): void {
         if (newState === true && oldState === false) {
             this.searchState.initValues();
+
+            // query から値をセット
+            if (typeof this.$route.query.keyword === 'string') {
+                this.searchState.keyword = this.$route.query.keyword;
+            }
+            if (typeof this.$route.query.ruleId !== 'undefined') {
+                this.searchState.ruleId = parseInt(this.$route.query.ruleId as string, 10);
+            }
+            if (typeof this.$route.query.channelId !== 'undefined') {
+                this.searchState.channelId = parseInt(this.$route.query.channelId as string, 10);
+            }
+            if (typeof this.$route.query.genre !== 'undefined') {
+                this.searchState.genre = parseInt(this.$route.query.genre as string, 10);
+            }
+            if (typeof this.$route.query.isOnlyOriginalFile !== 'undefined') {
+                this.searchState.isOnlyOriginalFile =
+                    (this.$route.query.isOnlyOriginalFile as any) === true ||
+                    this.$route.query.isOnlyOriginalFile === 'true';
+            }
         }
     }
 }
