7787543b0ca55ade01b51c8f7bc90f74b3b921be
l3tnun <l3tnun@gmail.com>
2020-08-12 23:00:04 +0900

    トップページに dashboard コンポーネントを追加 (表示内容の処理はまだ)

---
+++ client/src/components/dashboard/DashboardItem.vue
@@ -0,0 +1,43 @@
+<template>
+    <div class="dash-board-item pa-1">
+        <v-card class="item-content d-flex flex-column">
+            <div class="subtitle-1 px-4 py-2">{{ title }}</div>
+            <div class="content overflow-auto" ref="content" v-on:scroll="onScroll">
+                <slot name="items"></slot>
+            </div>
+        </v-card>
+    </div>
+</template>
+
+<script lang="ts">
+import { Component, Prop, Vue } from 'vue-property-decorator';
+
+@Component({})
+export default class DashboardItem extends Vue {
+    @Prop({ required: true })
+    public title!: string;
+
+    public onScroll(e: Event): void {
+        this.$emit('scroll', e);
+    }
+
+    /**
+     * スクロール位置をセットする
+     */
+    public setScrollTop(scrollTop: number): void {
+        if (typeof this.$refs.content === 'undefined') {
+            return;
+        }
+
+        (this.$refs.content as HTMLElement).scrollTop = scrollTop;
+    }
+}
+</script>
+
+<style lang="sass" scoped>
+.dash-board-item, .item-content
+    width: 100%
+    max-height: 100%
+    .content
+        max-height: 100%
+</style>
