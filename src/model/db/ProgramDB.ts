801766826536a55577592392d16eca96924d82e0
l3tnun <l3tnun@gmail.com>
2021-08-16 05:18:54 +0900

    AudioComponent の互換性向上

--- src/model/db/ProgramDB.ts
+++ src/model/db/ProgramDB.ts
@@ -220,9 +220,15 @@ export default class ProgramDB implements IProgramDB {
             value.videoComponentType = program.video.componentType;
         }
 
+        // audio
+        if (typeof (program as any).audio !== 'undefined') {
+            value.audioSamplingRate = (program as any).audio.samplingRate;
+            value.audioComponentType = (program as any).audio.componentType;
+        }
+
         // audios
-        if (typeof program.audios !== 'undefined') {
-            for (const audio of program.audios) {
+        if (typeof (program as any).audios !== 'undefined') {
+            for (const audio of (program as any).audios) {
                 // TODO 複数音声データに対応する
                 // 互換性維持のため main の音声情報だけを格納する
                 if (audio.isMain === false) {
