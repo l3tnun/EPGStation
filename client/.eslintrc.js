f699874ddbcd027676c17f5205e7348fe1542e0b
l3tnun <l3tnun@gmail.com>
2021-10-30 16:47:16 +0900

    update client packages

--- client/.eslintrc.js
+++ client/.eslintrc.js
@@ -20,7 +20,7 @@ module.exports = {
         parser: '@typescript-eslint/parser',
     },
     rules: {
-        'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+        'no-console': 'off',
         'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
         '@typescript-eslint/no-inferrable-types': 'off',
         '@typescript-eslint/no-explicit-any': 'off',
