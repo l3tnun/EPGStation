4fbd8a23a6f3a6fab8445c557a6ee3dd2097573f
T414CSP <113285117+T414CSP@users.noreply.github.com>
2022-09-11 19:47:36 +0900

    Hotfix: IPTV Simple Client is very slow.
    KODI Matrix 上の IPTV Simple Client で、チャンネルの切替に40秒ほどかかる問題に対する修正。
    https://github.com/l3tnun/EPGStation/issues/613 参照。

--- src/model/api/iptv/IPTVApiModel.ts
+++ src/model/api/iptv/IPTVApiModel.ts
@@ -58,6 +58,7 @@ class IPTVApiModel implements IIPTVApiModel {
             if (channel.hasLogoData) {
                 logo = `tvg-logo="${isSecure ? 'https' : 'http'}://${base}/api/channels/${channel.id}/logo"`;
             }
+            str += `#KODIPROP:mimetype=video/mp2t\n`;
             str += `#EXTINF:-1 tvg-id="${channel.id}" ${logo} group-title="${channel.channelType}",${channelName}　\n`;
             str += `${isSecure ? 'https' : 'http'}://${base}/api/streams/live/${channel.id}/m2ts?mode=${mode}\n`;
         }
